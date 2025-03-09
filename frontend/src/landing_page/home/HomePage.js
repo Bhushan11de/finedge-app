96e2e2ea66b73f18ea97f1b3230a1bbe00213768
Bhushan11de <138093209+Bhushan11de@users.noreply.github.com>
2025-03-09 23:03:45 +0530

    Update HomePage.js

--- frontend/src/landing_page/home/HomePage.js
+++ frontend/src/landing_page/home/HomePage.js
@@ -12,12 +12,28 @@ import OpenAccount from "../OpenAccount";
 function HomePage() {
   return (
     <>
+      {/* Header with the new app name */}
+      <header style={{ textAlign: 'center', margin: '50px 0' }}>
+        <h1>FINEDGE</h1> {/* Update with the new app name */}
+        <p>
+          Welcome to the best platform for growing your financial future. {/* Update this with relevant info */}
+        </p>
+      </header>
+
+      {/* Hero Section */}
       <Hero />
+
+      {/* Other sections */}
       <Awards />
       <Stats />
       <Pricing />
       <Education />
+
+      {/* Call to Action Section */}
       <OpenAccount />
+
+      {/* Footer */}
+      <Footer />
     </>
   );
 }
