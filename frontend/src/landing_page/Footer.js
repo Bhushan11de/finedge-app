d96f958703f12b81ee2c4d3696f257572763ceff
Bhushan11de <138093209+Bhushan11de@users.noreply.github.com>
2025-03-09 23:22:49 +0530

    Update Footer.js

--- frontend/src/landing_page/Footer.js
+++ frontend/src/landing_page/Footer.js
@@ -7,82 +7,90 @@ function Footer() {
         <div className="row mt-5">
           <div className="col">
             <img src="media/images/logo.svg" style={{ width: "50%" }} className="mb-3" />
-            <p className="mb-4">&copy; 2010 - 2024, Zerodha Broking Ltd. All rights reserved.</p>
+            <p className="mb-4">&copy; 2023 - 2024, Finedge. All rights reserved.</p>
             <div className="fs-4 d-flex gap-3 text-muted">
-              <i class="fa-brands fa-x-twitter"></i>
-              <i class="fa-brands fa-square-facebook"></i>
-              <i class="fa-brands fa-instagram"></i>
-              <i class="fa-brands fa-linkedin"></i>
+              <a href="https://x.com/sgbaustudents?lang=en" target="_blank" rel="noopener noreferrer">
+                <i className="fa-brands fa-x-twitter"></i>
+              </a>
+              <a href="https://www.facebook.com/santgadgebabaamravatiuniversity/" target="_blank" rel="noopener noreferrer">
+                <i className="fa-brands fa-square-facebook"></i>
+              </a>
+              <a href="https://www.instagram.com/sgbau_official/" target="_blank" rel="noopener noreferrer">
+                <i className="fa-brands fa-instagram"></i>
+              </a>
+              <a href="https://www.linkedin.com/school/sant-gadge-baba-amravati-university-amravati/posts/?feedView=all" target="_blank" rel="noopener noreferrer">
+                <i className="fa-brands fa-linkedin"></i>
+              </a>
             </div>
             <hr />
             <div className="fs-4 d-flex gap-3 text-muted">
-              <i class="fa-brands fa-youtube"></i>
-              <i class="fa-solid fa-comment"></i>
-              <i class="fa-brands fa-telegram"></i>
+              <a href="https://www.youtube.com/@santgadgebabaamravatiunive9624" target="_blank" rel="noopener noreferrer">
+                <i className="fa-brands fa-youtube"></i>
+              </a>
+              <a href="https://x.com/sgbaustudents?lang=en" target="_blank" rel="noopener noreferrer">
+                <i className="fa-brands fa-telegram"></i>
+              </a>
             </div>
           </div>
           <div className="col footer-nav">
             <p>Company</p>
-            <a href="">About</a>
+            <a href="/about">About</a>
             <br />
-            <a href="">Products</a>
+            <a href="/products">Products</a>
             <br />
-            <a href="">Pricing</a>
+            <a href="/pricing">Pricing</a>
             <br />
-            <a href="">Referral programme</a>
+            <a href="/referral">Referral programme</a>
             <br />
-            <a href="">Careers</a>
+            <a href="/careers">Careers</a>
             <br />
-            <a href="">Zerodha.tech</a>
+            <a href="/tech">Finedge.tech</a>
             <br />
-            <a href="">Press & media</a>
+            <a href="/press-media">Press & media</a>
             <br />
-            <a href="">Zerodha Cares (CSR)</a>
+            <a href="/csr">CSR</a>
             <br />
           </div>
           <div className="col footer-nav">
             <p>Support</p>
-            <a href="">Contact us</a>
+            <a href="/contact">Contact us</a>
             <br />
-            <a href="">Support portal</a>
+            <a href="/support-portal">Support portal</a>
             <br />
-            <a href="">Z-Connect blog</a>
+            <a href="/blog">Blog</a>
             <br />
-            <a href="">List of charges</a>
+            <a href="/charges">List of charges</a>
             <br />
-            <a href="">Downloads & resources</a>
+            <a href="/resources">Downloads & resources</a>
             <br />
           </div>
           <div className="col footer-nav">
             <p>Account</p>
-            <a href="">Open an account</a>
-            <a href="">Fund transfer</a>
+            <a href="/open-account">Open an account</a>
+            <br />
+            <a href="/fund-transfer">Fund transfer</a>
           </div>
         </div>
         <div className="mt-5 text-muted" style={{ fontSize: "14px" }}>
           <p>
-            Zerodha Broking Ltd.: Member of NSE, BSE​ &​ MCX – SEBI Registration no.: INZ000031633 CDSL/NSDL: Depository services through Zerodha Broking Ltd. – SEBI Registration no.: IN-DP-431-2019 Commodity Trading through Zerodha Commodities Pvt. Ltd. MCX: 46025; NSE-50001 – SEBI Registration no.: INZ000038238 Registered Address: Zerodha Broking Ltd., #153/154, 4th Cross, Dollars Colony, Opp. Clarence Public School, J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka, India. For any complaints pertaining to securities broking please write to complaints@zerodha.com, for DP related to dp@zerodha.com. Please ensure you carefully read the Risk Disclosure Document as prescribed by SEBI | ICF Procedure to file a complaint on SEBI SCORES: Register on SCORES portal. Mandatory details for filing complaints on SCORES: Name, PAN, Address, Mobile Number, E-mail ID. Benefits: Effective Communication, Speedy redressal of the grievances Smart Online Dispute Resolution | Grievances Redressal
-            Mechanism
+            [Your App Name]: Member of NSE, BSE​ &​ MCX – SEBI Registration no.: INZ000031633 CDSL/NSDL: Depository services through [Your App Name] – SEBI Registration no.: IN-DP-431-2019 Commodity Trading through [Your App Name] – MCX: 46025; NSE-50001 – SEBI Registration no.: INZ000038238 Registered Address: [Your Address Here], Bengaluru - 560078, Karnataka, India. For any complaints, please write to complaints@yourapp.com.
           </p>
           <br />
           <p>
-            Investments in securities market are subject to market risks; read all the related documents carefully before investing. Attention investors: 1) Stock brokers can accept securities as margins from clients only by way of pledge in the depository system w.e.f September 01, 2020. 2) Update your e-mail and phone number with your stock broker / depository participant and receive OTP directly from depository on your e-mail and/or mobile number to create pledge. 3) Check your securities / MF / bonds in the consolidated account statement issued by NSDL/CDSL every month. "Prevent unauthorised transactions in your account. Update your mobile numbers/email IDs with your stock brokers. Receive information of your transactions directly from Exchange on your mobile/email at the end of the day. Issued in the interest of investors. KYC is one time exercise while dealing in securities markets - once KYC is done through a SEBI registered intermediary (broker, DP, Mutual Fund etc.), you need
-            not
+            Investments in securities market are subject to market risks; read all the related documents carefully before investing. Attention investors: 1) Stock brokers can accept securities as margins from clients only by way of pledge in the depository system w.e.f September 01, 2020. 2) Update your e-mail and phone number with your stock broker / depository participant and receive OTP directly from depository on your e-mail and/or mobile number to create pledge. 3) Check your securities / MF / bonds in the consolidated account statement issued by NSDL/CDSL every month.
           </p>
           <br />
-          <p>undergo the same process again when you approach another intermediary." Dear Investor, if you are subscribing to an IPO, there is no need to issue a cheque. Please write the Bank account number and sign the IPO application form to authorize your bank to make payment in case of allotment. In case of non allotment the funds will remain in your bank account. As a business we don't give stock tips, and have not authorized anyone to trade on behalf of others. If you find anyone claiming to be part of Zerodha and offering such services, please create a ticket here.</p>
+          <p>
+            As a business, we don't give stock tips and have not authorized anyone to trade on behalf of others. If you find anyone claiming to be part of [Your App Name] and offering such services, please create a ticket here.
+          </p>
           <br />
         </div>
         <div className="mt-2 text-muted d-flex gap-3 justify-content-center footer-links" style={{ fontSize: "15px" }}>
-          <p>NSE</p>
-          <p>BSE</p>
-          <p>MCX</p>
-          <p>Terms & Conditions</p>
+          <a href="/terms">Terms & Conditions</a>
           <p>Policies and procedures</p>
-          <p>Privacy Policy </p>
-          <p>Disclosure</p>
-          <p>For investors Attention</p>
-          <p>Investors charter</p>
+          <p><a href="/privacy">Privacy Policy</a></p>
+          <p><a href="/disclosure">Disclosure</a></p>
+          <p><a href="/investors-charter">Investors Charter</a></p>
         </div>
       </div>
     </footer>
