ec31b03345ca3069b3448ba7b463f6597116ec78
Bhushan11de <138093209+Bhushan11de@users.noreply.github.com>
2025-03-09 23:29:34 +0530

    Update Navbar.js

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -6,7 +6,10 @@ function Navbar() {
     <nav className="navbar navbar-expand-lg border-bottom" style={{ backgroundColor: "#FFF" }}>
       <div className="container p-2">
         <Link className="navbar-brand" to="/">
+          {/* Update the logo path if necessary */}
           <img src="media/images/logo.svg" alt="logo" style={{ width: "25%" }} />
+          {/* Optionally add the app name text */}
+          {/* <span>FINEDGE</span> */}
         </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -15,7 +18,7 @@ function Navbar() {
           <form className="d-flex" role="search">
             <ul className="navbar-nav mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="https://zerodha-clone-dashboard.vercel.app/register">
+                <a className="nav-link active" aria-current="page" href="https://[your-app-url]/register">
                   Register
                 </a>
               </li>
@@ -41,7 +44,7 @@ function Navbar() {
               </li>
               <li className="nav-item active">
                 <Link className="nav-link" to="/">
-                  <i class="fa-solid fa-bars"></i>
+                  <i className="fa-solid fa-bars"></i>
                 </Link>
               </li>
             </ul>
