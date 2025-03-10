9db1d284f48cd4fbfa1d4fe2793c7f82bfc72ccb
Bhushan11de <138093209+Bhushan11de@users.noreply.github.com>
2025-03-10 10:12:24 +0530

    Update Education.js

--- frontend/src/landing_page/home/Education.js
+++ frontend/src/landing_page/home/Education.js
@@ -13,4 +13,21 @@ function Education() {
           <p>
             Varsity, the largest online stock market education book in the world covering everything from the basics to advanced trading.
           </p>
-          {/* Provided valid href link or use button if no link is availabl
+          {/* Provided valid href link or use button if no link is available */}
+          <a href="https://www.varsity.in/" target="_blank" style={{ textDecoration: "none" }} rel="noopener noreferrer">
+            varsity <i className="fa-solid fa-arrow-right-long"></i>
+          </a>
+          <p className="mt-5">
+            TradingQ&A, the most active trading and investment community in India for all your market-related queries.
+          </p>
+          {/* Provided valid href link or use button if no link is available */}
+          <a href="https://www.tradingqna.com/" target="_blank" style={{ textDecoration: "none" }} rel="noopener noreferrer">
+            TradingQ&A <i className="fa-solid fa-arrow-right-long"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Education;
