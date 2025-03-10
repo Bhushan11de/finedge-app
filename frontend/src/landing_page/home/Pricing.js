074321ab40157ca9f8f7d65ba5ae55a2720320a3
Bhushan11de <138093209+Bhushan11de@users.noreply.github.com>
2025-03-10 10:17:36 +0530

    Update Pricing.js

--- frontend/src/landing_page/home/Pricing.js
+++ frontend/src/landing_page/home/Pricing.js
@@ -1,15 +1,29 @@
 import React from "react";
 
 function Pricing() {
+  const handlePricingClick = () => {
+    // Replace this with the desired action or functionality
+    alert("Redirecting to Pricing details");
+  };
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-4">
           <h1 className="mb-3 fs-2">Unbeatable pricing</h1>
           <p>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
-          <a href="" style={{ textDecoration: "none" }}>
-            See pricing <i class="fa-solid fa-arrow-right-long"></i>
-          </a>
+          <button
+            style={{
+              textDecoration: "none",
+              background: "none",
+              border: "none",
+              color: "#387ed1",
+              cursor: "pointer",
+            }}
+            onClick={handlePricingClick}
+          >
+            See pricing <i className="fa-solid fa-arrow-right-long"></i>
+          </button>
         </div>
         <div className="col-2"></div>
         <div className="col-6 mb-5">
