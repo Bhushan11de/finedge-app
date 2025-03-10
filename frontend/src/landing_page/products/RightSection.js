3c585d4b11ad1b35409b59a92b3f353a30c4d7b3
Bhushan11de <138093209+Bhushan11de@users.noreply.github.com>
2025-03-10 10:28:42 +0530

    Update RightSection.js

--- frontend/src/landing_page/products/RightSection.js
+++ frontend/src/landing_page/products/RightSection.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function RightSection({ imageUrl, productName, productDescrption, learnMore }) {
+function RightSection({ imageUrl, productName, productDescrption, learnMore, imageAlt }) {
   return (
     <div className="container">
       <div className="row">
@@ -8,11 +8,11 @@ function RightSection({ imageUrl, productName, productDescrption, learnMore }) {
           <h1>{productName}</h1>
           <p>{productDescrption}</p>
           <a href={learnMore} style={{ textDecoration: "none", color: "#387ed1" }}>
-            Learn More <i class="fa-solid fa-arrow-right-long"></i>
+            Learn More <i className="fa-solid fa-arrow-right-long"></i>
           </a>
         </div>
         <div className="col-6">
-          <img src={imageUrl} />
+          <img src={imageUrl} alt={imageAlt} />
         </div>
       </div>
     </div>
