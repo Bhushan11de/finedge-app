6dda112d10f01614608399e604f3b856227e8acd
Bhushan11de <138093209+Bhushan11de@users.noreply.github.com>
2025-03-11 14:41:49 +0530

    Update LeftSection.js

--- frontend/src/landing_page/products/LeftSection.js
+++ frontend/src/landing_page/products/LeftSection.js
@@ -5,25 +5,25 @@ function LeftSection({ imageUrl, productName, productDesrption, tryDemo, learnMo
     <div className="container mt-5">
       <div className="row">
         <div className="col-6 p-5">
-          <img src={imageUrl} />
+          <img src={imageUrl} alt={productName} />
         </div>
         <div className="col-6 p-5 mt-5">
           <h1>{productName}</h1>
           <p>{productDesrption}</p>
           <div>
             <a href={tryDemo} style={{ textDecoration: "none", color: "#387ed1" }}>
-              Try Demo <i class="fa-solid fa-arrow-right-long"></i>
+              Try Demo <i className="fa-solid fa-arrow-right-long"></i>
             </a>
             <a href={learnMore} style={{ marginLeft: "50px", textDecoration: "none", color: "#387ed1" }}>
-              Learn More <i class="fa-solid fa-arrow-right-long"></i>
+              Learn More <i className="fa-solid fa-arrow-right-long"></i>
             </a>
           </div>
           <div className="mt-3">
             <a href={googlePlay}>
-              <img src="media/images/googlePlayBadge.svg" />
+              <img src="media/images/googlePlayBadge.svg" alt="Get it on Google Play" />
             </a>
             <a href={appStore} style={{ marginLeft: "50px" }}>
-              <img src="media/images/appstoreBadge.svg" />
+              <img src="media/images/appstoreBadge.svg" alt="Download on the App Store" />
             </a>
           </div>
         </div>
