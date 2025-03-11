08dd56b57302df1f79488f6910aacd5034b592a6
Bhushan11de <138093209+Bhushan11de@users.noreply.github.com>
2025-03-11 14:37:48 +0530

    Update Hero.js

--- frontend/src/landing_page/about/Hero.js
+++ frontend/src/landing_page/about/Hero.js
@@ -10,14 +10,17 @@ function Hero() {
       </div>
       <div className="row p-5 mt-5 border-top text-muted lh-lg" style={{ fontSize: "1.2em" }}>
         <div className="col-6 p-5">
-          <p>We kick-started operations on the 15th of August, 2010 with the goal of breaking all barriers that traders and investors face in India in terms of cost, support, and technology. We named the company Zerodha, a combination of Zero and "Rodha", the Sanskrit word for barrier.</p>
+          <p>
+            We kick-started operations on the 15th of August, 2010 with the goal of breaking all barriers that traders and investors face in India in terms of cost, support, and technology. 
+            We named the company Finedge, a combination of "Fine" and "Edge", symbolizing a sharp and efficient financial experience.
+          </p>
           <p>Today, our disruptive pricing models and in-house technology have made us the biggest stock broker in India.</p>
           <p>Over 1+ Crore clients place millions of orders every day through our powerful ecosystem of investment platforms, contributing over 15% of all Indian retail trading volumes.</p>
         </div>
         <div className="col-6 p-5">
           <p>In addition, we run a number of popular open online educational and community initiatives to empower retail traders and investors.</p>
           <p>
-            <a href="https://rainmatter.com/" target="_blank" style={{ textDecoration: "none", color: "#387ed1" }}>
+            <a href="https://rainmatter.com/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none", color: "#387ed1" }}>
               Rainmatter
             </a>
             , our fintech fund and incubator, has invested in several fintech startups with the goal of growing the Indian capital markets.
