3650527b63953dba5bda5234347a85e82884dab4
Bhushan11de <138093209+Bhushan11de@users.noreply.github.com>
2025-03-11 20:18:13 +0530

    Update Signup.js

--- frontend/src/landing_page/signup/Signup.js
+++ frontend/src/landing_page/signup/Signup.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Register from "../register/Register"; // ✅ Corrected import path
+import Register from "../register/Register"; // ✅ Absolute path use karein
 
 function Signup() {
   return <Register />; // Register component ko render kar raha hai
