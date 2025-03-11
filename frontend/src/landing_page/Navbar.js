dcdf89277a615b466fe34d74a9b7c8579f9db874
Bhushan11de <138093209+Bhushan11de@users.noreply.github.com>
2025-03-11 17:23:53 +0530

    Update Navbar.js

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -1,25 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../landing_page/images/Finedgelogo.jpg"; // ✅ Sahi Path
+import logo from "../landing_page/images/Finedgelogo.jpg"; // ✅ सही Path
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg border-bottom" style={{ backgroundColor: "#FFF" }}>
       <div className="container p-2">
         <Link className="navbar-brand" to="/">
-          {/* ✅ Updated Logo Path */}
           <img src={logo} alt="Finedge Logo" style={{ width: "25%" }} />
         </Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <form className="d-flex" role="search">
             <ul className="navbar-nav mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="https://zerodha-clone-dashboard.vercel.app/register">
+                <Link className="nav-link active" aria-current="page" to="/register">
                   Register
-                </a>
+                </Link>
               </li>
               <li className="nav-item active">
                 <Link className="nav-link" to="/about">
@@ -43,7 +50,7 @@ function Navbar() {
               </li>
               <li className="nav-item active">
                 <Link className="nav-link" to="/">
-                  <i className="fa-solid fa-bars"></i> {/* ✅ className Fix */}
+                  <i className="fa-solid fa-bars"></i>
                 </Link>
               </li>
             </ul>
