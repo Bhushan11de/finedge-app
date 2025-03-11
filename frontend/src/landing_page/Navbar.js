a48420358bd35538cb27350e1ae6f411cbec407f
Bhushan11de <138093209+Bhushan11de@users.noreply.github.com>
2025-03-11 18:27:21 +0530

    Update Navbar.js

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -1,24 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "./images/Finedgelogo.jpg"; // ✅ Correct Logo Path
+import logo from "../landing_page/images/Finedgelogo.jpg"; // ✅ Logo Path Verify करो
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg border-bottom" style={{ backgroundColor: "#FFF" }}>
       <div className="container p-2">
         <Link className="navbar-brand" to="/">
-          <img src={logo} alt="Finedge Logo" style={{ width: "25%" }} />
+          <img src={logo} alt="Finedge Logo" style={{ width: "150px" }} />
         </Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/register">
-                Register
-              </Link>
-            </li>
+          <ul className="navbar-nav ms-auto mb-lg-0">
             <li className="nav-item">
               <Link className="nav-link" to="/about">About</Link>
             </li>
@@ -32,8 +35,8 @@ function Navbar() {
               <Link className="nav-link" to="/support">Support</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/">
-                <i className="fa-solid fa-bars"></i>
+              <Link className="nav-link btn btn-primary text-white" to="/register">
+                Register
               </Link>
             </li>
           </ul>
@@ -44,4 +47,3 @@ function Navbar() {
 }
 
 export default Navbar;
-
