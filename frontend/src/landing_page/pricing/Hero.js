4260464bd13bc58955c3c7c64dcd635a44d50220
Bhushan11de <138093209+Bhushan11de@users.noreply.github.com>
2025-03-11 14:27:14 +0530

    Update Hero.js

--- frontend/src/landing_page/pricing/Hero.js
+++ frontend/src/landing_page/pricing/Hero.js
@@ -5,23 +5,41 @@ function Hero() {
     <div className="container">
       <div className="row p-5 mt-5 border-bottom text-center">
         <h1>Pricing</h1>
-        <h3 className="text-muted fs-4 mt-2">Free equity investments and flat ₹20 intraday and F&O trades</h3>
+        <h3 className="text-muted fs-4 mt-2">
+          Free equity investments and flat ₹20 intraday and F&O trades
+        </h3>
       </div>
       <div className="row p-5 mt-5 text-center">
         <div className="col-4 p-4">
-          <img src="media/images/pricingEquity.svg" />
+          <img
+            src="media/images/pricingEquity.svg"
+            alt="Equity pricing"
+          />
           <h1 className="fs-3">Free equity delivery</h1>
-          <p className="text-muted mt-2">All equity delivery investments (NSE, BSE), are absolutely free — ₹ 0 brokerage.</p>
+          <p className="text-muted mt-2">
+            All equity delivery investments (NSE, BSE) are absolutely free — ₹0 brokerage.
+          </p>
         </div>
         <div className="col-4 p-4">
-          <img src="media/images/intradayTrades.svg" />
+          <img
+            src="media/images/intradayTrades.svg"
+            alt="Intraday and F&O trades"
+          />
           <h1 className="fs-3">Intraday and F&O trades</h1>
-          <p className="text-muted mt-2">Flat ₹ 20 or 0.03% (whichever is lower) per executed order on intraday trades across equity, currency, and commodity trades. Flat ₹20 on all option trades.</p>
+          <p className="text-muted mt-2">
+            Flat ₹20 or 0.03% (whichever is lower) per executed order on intraday trades across equity, 
+            currency, and commodity trades. Flat ₹20 on all option trades.
+          </p>
         </div>
         <div className="col-4 p-4">
-          <img src="media/images/pricingEquity.svg" />
+          <img
+            src="media/images/pricingEquity.svg"
+            alt="Mutual funds pricing"
+          />
           <h1 className="fs-3">Free equity delivery</h1>
-          <p className="text-muted mt-2">All direct mutual fund investments are absolutely free — ₹ 0 commissions & DP charges.</p>
+          <p className="text-muted mt-2">
+            All direct mutual fund investments are absolutely free — ₹0 commissions & DP charges.
+          </p>
         </div>
       </div>
     </div>
