f2a29741dd289ac8e8bbec65a435207b37180086
Bhushan11de <138093209+Bhushan11de@users.noreply.github.com>
2025-03-11 14:33:16 +0530

    Update Stats.js

--- frontend/src/landing_page/home/Stats.js
+++ frontend/src/landing_page/home/Stats.js
@@ -7,22 +7,22 @@ function Stats() {
         <div className="col-6 p-5">
           <h1 className="fs-2 mb-5">Trust with confidence</h1>
           <h2 className="fs-4">Customer-first always</h2>
-          <p className="text-muted">That's why 1.5+ crore customers trust Zerodha with ₹4.5+ lakh crores worth of equity investments.</p>
+          <p className="text-muted">That's why 1.5+ crore customers trust Finedge with ₹4.5+ lakh crores worth of equity investments.</p>
           <h2 className="fs-4">No spam or gimmicks</h2>
-          <p className="text-muted">No gimmicks, spam, "gamification", or annoying push notifications. High quality apps that you use at your pace, the way you like.</p>
-          <h2 className="fs-4">The Zerodha universe</h2>
+          <p className="text-muted">No gimmicks, spam, "gamification", or annoying push notifications. High-quality apps that you use at your pace, the way you like.</p>
+          <h2 className="fs-4">The Finedge universe</h2>
           <p className="text-muted">Not just an app, but a whole ecosystem. Our investments in 30+ fintech startups offer you tailored services specific to your needs.</p>
           <h2 className="fs-4">Do better with money</h2>
           <p className="text-muted">With initiatives like Nudge and Kill Switch, we don't just facilitate transactions, but actively help you do better with your money.</p>
         </div>
         <div className="col-6 p-5">
-          <img src="media/images/ecosystem.png" style={{ width: "90%" }} />
+          <img src="media/images/ecosystem.png" alt="Finedge Ecosystem" style={{ width: "90%" }} />
           <div className="text-center">
-            <a href="" className="mx-5" style={{ textDecoration: "none" }}>
-              Explore ours products <i class="fa-solid fa-arrow-right-long"></i>
+            <a href="#" className="mx-5" style={{ textDecoration: "none" }}>
+              Explore our products <i className="fa-solid fa-arrow-right-long"></i>
             </a>
-            <a href="" style={{ textDecoration: "none" }}>
-              Try Kite Demo <i class="fa-solid fa-arrow-right-long"></i>
+            <a href="#" style={{ textDecoration: "none" }}>
+              Try Kite Demo <i className="fa-solid fa-arrow-right-long"></i>
             </a>
           </div>
         </div>
