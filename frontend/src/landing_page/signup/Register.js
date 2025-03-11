91d5736251b3449b6a345a422f6ad2a4428bdce9
Bhushan11de <bd11121998@gmail.com>
2025-03-11 14:52:12 +0000

    change in signup

---
+++ frontend/src/landing_page/signup/Register.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Register() {
+  return (
+    <div>
+      <h1>Register</h1>
+      <p>Sign up for Finedge here.</p>
+    </div>
+  );
+}
+
+export default Register;
+
