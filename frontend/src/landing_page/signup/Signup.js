e0febdb6069b93e6b5ccb00b586b4c569a76e771
Bhushan11de <138093209+Bhushan11de@users.noreply.github.com>
2025-03-11 19:59:25 +0530

    Update Signup.js

--- frontend/src/landing_page/signup/Signup.js
+++ frontend/src/landing_page/signup/Signup.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Register from "./Register"; // Register component ko import kar rahe hain
 
 function Signup() {
-  return <h1>Signup</h1>;
+  return <Register />; // Register component ko render kar rahe hain
 }
 
 export default Signup;
